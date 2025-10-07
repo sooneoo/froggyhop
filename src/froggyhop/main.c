f61938c4e9baf492f5ae7f870384a33d4cd445fe
Petr Horáček <phoracek.edit@gmail.com>
2025-10-07 13:03:04 +0200

    0.1.0 - base GameState architecture, version

--- src/froggyhop/main.c
+++ src/froggyhop/main.c
@@ -3,18 +3,155 @@
 #include <stdbool.h>
 #include <raylib.h>
 
-
-typedef struct GameManager_Context GameManager_Context;
-
-
-typedef struct GameState {
-	struct GameState * (*callback)(struct GameState *, GameManager_Context *, float);
-} GameState;
+typedef struct GameState GameState;
 
 
-struct GameManager_Context {
+typedef struct {
 	GameState ** state_stack;
+} GameManager_Context;
+
+
+typedef enum {
+    GameState_ID_Menu
+    , GameState_ID_Options
+    , GameState_ID_Pause
+    , GameState_ID_GameControl
+    , GameState_ID_GameOver
+    , GameState_ID_N
+} GameState_ID ;
+
+
+struct GameState {
+	struct GameState * (*callback)(struct GameState *, GameManager_Context *, float);
 };
+
+
+typedef struct {
+    GameState super;
+} GameState_Menu;
+
+
+#define GAME_STATE_MENU ((T)(GameState_Menu*))
+
+
+static GameState * game_state_menu_callback(
+        GameState * self, GameManager_Context * context, float frame_time) {
+    DrawText("Menu", 400, 300, 40, BLACK);
+    if(IsKeyPressed(KEY_SPACE) == true) {
+        return context->state_stack[GameState_ID_Options];        
+    } else {
+        return context->state_stack[GameState_ID_Menu];        
+    }
+}
+
+
+GameState_Menu game_state_menu(void) {
+    return (GameState_Menu) {
+        .super = {
+            .callback = game_state_menu_callback
+        }
+    };
+}
+
+
+typedef struct {
+    GameState super;
+} GameState_Options;
+
+
+static GameState * game_state_options_callback(
+        GameState * self, GameManager_Context * context, float frame_time) {
+    DrawText("Options", 400, 300, 40, BLACK);
+    if(IsKeyPressed(KEY_SPACE) == true) {
+        return context->state_stack[GameState_ID_GameControl];
+    } else {
+        return context->state_stack[GameState_ID_Options];
+    }
+}
+
+
+GameState_Options game_state_options(void) {
+    return (GameState_Options) {
+        .super = {
+            .callback = game_state_options_callback
+        }     
+    };
+}
+
+
+typedef struct {
+    GameState super;
+} GameState_Pause;
+
+
+static GameState * game_state_pause_callback(
+        GameState * self, GameManager_Context * context, float frame_time) {
+    DrawText("Pause", 400, 300, 40, BLACK);
+    if(IsKeyPressed(KEY_SPACE) == true) {
+        return context->state_stack[GameState_ID_GameOver];
+    } else {
+        return context->state_stack[GameState_ID_Pause];
+    }
+}
+
+
+GameState_Pause game_state_pause(void) {
+    return (GameState_Pause) {
+        .super = {
+            .callback = game_state_pause_callback
+        }
+    };
+}
+
+
+typedef struct {
+    GameState super;
+} GameState_GameControl;
+
+
+static GameState * game_state_game_control_callback(
+        GameState * self, GameManager_Context * context, float frame_time) {
+    DrawText("Game Control", 400, 300, 40, BLACK);
+    if(IsKeyPressed(KEY_SPACE) == true) {
+        return context->state_stack[GameState_ID_Pause];
+    } else {
+        return context->state_stack[GameState_ID_GameControl];
+    }
+}
+
+
+GameState_GameControl game_state_game_control(void) {
+    return (GameState_GameControl) {
+        .super = {
+            .callback = game_state_game_control_callback
+        }
+    };
+}
+
+
+typedef struct {
+    GameState super;
+} GameState_GameOver;
+
+
+static GameState * game_state_game_over_callback(
+        GameState * self, GameManager_Context * context, float frame_time) {
+    DrawText("Game Over", 400, 300, 40, BLACK);
+    if(IsKeyPressed(KEY_SPACE) == true) {
+        return context->state_stack[GameState_ID_Menu];
+    } else {
+        return context->state_stack[GameState_ID_GameOver];
+    }
+}
+
+ 
+GameState_GameOver game_state_game_over(void) {
+    return (GameState_GameOver) {
+        .super = {
+            .callback = game_state_game_over_callback
+        }
+    };
+}
 
 
 typedef struct {
@@ -39,10 +176,24 @@ void game_manager_execute(GameManager * self) {
 }
 
 
+static GameState_Menu state_menu;
+static GameState_Options state_options;
+static GameState_GameControl state_game_control;
+static GameState_Pause state_pause;
+static GameState_GameOver state_game_over;
+
+static GameState* state_buff_mem[GameState_ID_N] = {
+    &state_menu.super
+    , &state_options.super
+    , &state_pause.super
+    , &state_game_control.super
+    , &state_game_over.super
+};
+
+GameManager game_manager_instance;
+
+
 #define WINDOW_TITLE "Froggy Hop"
-
-
-
 
 
 int main(void) {
@@ -50,11 +201,18 @@ int main(void) {
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 	SetTargetFPS(144);
 	
-	GameManager game_manager_instance = game_manager(NULL, NULL);
+    
+    state_menu = game_state_menu();
+    state_options = game_state_options();
+    state_game_control = game_state_game_control();
+    state_pause = game_state_pause();
+    state_game_over = game_state_game_over();
+	game_manager_instance = game_manager(&state_menu.super, state_buff_mem);
 
 	while(WindowShouldClose() == false) {
 		BeginDrawing();
 		ClearBackground(WHITE);
+        game_manager_execute(&game_manager_instance);
 		DrawFPS(10, 10);
 		EndDrawing();
 	}
---
+++ src/froggyhop/version.h
@@ -0,0 +1,19 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+
+typedef struct {
+    unsigned short major;
+    unsigned short minor;
+    unsigned short patch;    
+} FroggyHop_Version;
+
+
+#define froggyhop_version() \
+    (FroggyHop_Version) {   \
+        .major = 0          \
+        , .minor = 1        \
+        , .patch = 0        \
+}
+
+#endif
